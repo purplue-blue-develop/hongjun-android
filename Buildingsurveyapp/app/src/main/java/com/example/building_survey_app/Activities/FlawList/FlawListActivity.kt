37cb1a966ef5f63eddbc1ad37648a87e4ae9bcc3
Jeong Yo Han <ingyeoking13@gmail.com>
2020-06-28 13:09:55 +0900

    저장시퀀스 작업완료 엑셀헬퍼 클래스 생성

--- Buildingsurveyapp/app/src/main/java/com/example/building_survey_app/Activities/FlawList/FlawListActivity.kt
+++ Buildingsurveyapp/app/src/main/java/com/example/building_survey_app/Activities/FlawList/FlawListActivity.kt
@@ -3,10 +3,12 @@ package com.example.building_survey_app.Activities.FlawList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.Environment.*
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ListView
+import com.example.building_survey_app.Helper.ExcelHelper
 import com.example.building_survey_app.ListViewFlawItem
 import com.example.building_survey_app.ListViewFlawItemAdapter
 import com.example.building_survey_app.R
@@ -15,11 +17,9 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.poifs.filesystem.POIFSFileSystem
 import org.apache.poi.ss.usermodel.Sheet
 import org.apache.poi.ss.usermodel.Workbook
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
+import java.io.*
 import java.lang.Exception
+import kotlin.system.exitProcess
 
 class FlawListActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -58,42 +58,65 @@ class FlawListActivity : AppCompatActivity(), View.OnClickListener {
 
     fun SaveExcel()
     {
-        try {
+        var excelHelper =  ExcelHelper();
+        excelHelper.Write(BuildingProjectListViewModel.BuildingProjectList[0].flawList);
+//        try {
+//            var testFile = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "씨브럴.txt");
+//            testFile.createNewFile();
+//
+//        var writer =BufferedWriter(FileWriter(testFile, true));
+//        writer.append("SIBAL");
+//        writer.newLine();
+//        writer.append("SIBAL");
+//        writer.close();
+//
+//        var state = getExternalStorageState();
+//        if ( state != "mounted")
+//        {
+//            exitProcess(-1);
+//        }
+//        testFile.createNewFile();
+
             var file= File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "결함정보.xls");
 
-            var wb : Workbook?;
-            var sheet : Sheet?;
+            var wb : Workbook? = null;
+            var sheet : Sheet? = null;
+
             if (file.exists())
             {
                 wb= HSSFWorkbook();
-                sheet = wb.createSheet();
+                wb.createSheet();
+                sheet = wb.getSheetAt(0);
             }
             else
             {
                 file.createNewFile();
-                var fileSystem = POIFSFileSystem(file.inputStream())
-                wb = HSSFWorkbook(fileSystem);
-                wb.removeSheetAt(0);
-                wb.createSheet();
-                sheet = HSSFWorkbook(fileSystem).getSheetAt(0);
+                var bytes = file.inputStream().readBytes()
+                if (bytes.isNotEmpty()){
+                    var fileSystem = POIFSFileSystem(file.inputStream())
+                    wb = HSSFWorkbook(fileSystem);
+                    wb.removeSheetAt(0);
+                    wb.createSheet();
+                    sheet = wb.getSheetAt(0);
+                }
             }
+            val row = sheet?.createRow(0);
+            var cell = row?.createCell(0);
+            row?.getCell(0)?.setCellValue(11.0);
+            row?.createCell(1);
+            row?.getCell(1)?.setCellValue(12.0);
 
-            val row = sheet.createRow(0);
-            var cell = row.createCell(0);
-            cell.setCellValue("하이");
-
-            file = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "결함정보.xls");
-            try {
-                var os = FileOutputStream(file);
-                wb.write(os);
-            }
-            catch (e : IOException){
-                e.printStackTrace();
-            }
-        }
-        catch (e : Exception)
-        {
-            Log.e("Save Sequence Error", e.toString());
-        }
+//            try {
+                var os = FileOutputStream(file.absolutePath);
+                wb?.write(os);
+//            }
+//            catch (e : IOException){
+//                e.printStackTrace();
+//            }
+//        }
+//        catch (e : Exception)
+//        {
+//            Log.e("Save Sequence Error", e.toString());
+//        }
     }
 }
---
+++ Buildingsurveyapp/app/src/main/java/com/example/building_survey_app/Helper/ExcelHelper.kt
@@ -0,0 +1,17 @@
+package com.example.building_survey_app.Helper
+
+import com.example.building_survey_app.Models.FlawModel
+
+class ExcelHelper {
+
+    fun ExcelHelper()
+    {
+
+    }
+
+    public fun Write(mutableList: MutableList<FlawModel>)
+    {
+
+    }
+
+}
